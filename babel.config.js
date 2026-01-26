f214bfb6cf3366b00a3050f25693f0a43fc03879
Miguel Olteanu <miguelolte2003@gmail.com>
2026-01-26 15:47:57 +0100

    feat: add react-native-reanimated plugin to Babel configuration for improved animation support

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,19 @@
 // babel.config.js
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        [
-          'babel-preset-expo',
-          {
-            // Asegura que las transformaciones sean compatibles con Hermes
-            jsxRuntime: 'automatic',
-          },
-        ],
+  api.cache(true);
+  return {
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          // Mantenemos tu configuración actual de JSX
+          jsxRuntime: 'automatic',
+        },
       ],
-      plugins: [],
-    };
+    ],
+    plugins: [
+      // ESTA LÍNEA ES LA QUE FALTA:
+      'react-native-reanimated/plugin',
+    ],
   };
+};
